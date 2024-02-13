27d5424aaa53f7a0e4de36091c463a75a3045b25
naneeta1 <nainatalreja3@gmail.com>
2024-02-13 20:38:34 +0500

    change url

--- SRC/Config/index.js
+++ SRC/Config/index.js
@@ -1,11 +1,11 @@
-export const baseUrl = "https://roy-api.ad-wize.com";
+export const baseUrl = "https://royapi.ad-wize.net";
 // export const baseUrl = "https://4b54-113-203-241-1.in.ngrok.io";
 export const imageUrl = `https://roy.ad-wize.com`;
 export const imageSizeUrl =`https://roy.ad-wize.com/uploads/product/size`;
 export const profilePicUrl = `${baseUrl}/uploads`
 
 export const apiDataLimit = 10;
-export const validateEmail = (email) => {
+export const validateEmail = (email) => { 
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
